3bd35f8c0b6ee5887ae083c800f58609c5ba140f
DeusMalsith <justinrashallphotography@gmail.com>
2021-05-26 03:43:00 -0500

    Added navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import logo from '../images/shared/desktop/logo.svg';
+import * as navbarStyles from '../components/Navbar.module.css';
 
 function Navbar() {
   return (
     <nav>
-      <ul>
-        <li>
+      <ul className={navbarStyles.navbar}>
+        <li className={navbarStyles.logo}>
+          <img src={logo} alt='coffeeroasters logo' />
+        </li>
+        <li className={navbarStyles.navLinks}>
           <Link to='/'>Home</Link>
         </li>
-        <li>
+        <li className={navbarStyles.navLinks}>
           <Link to='/about'>About Us</Link>
         </li>
-        <li>
+        <li className={navbarStyles.navLinks}>
           <Link to='/plan'>Create your plan</Link>
         </li>
       </ul>
