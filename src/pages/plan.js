b3b10951588cff830d6b6d29c146904af113a14b
Justin <41171100+DeusMalsith@users.noreply.github.com>
2021-11-16 17:53:00 -0600

    Update plan.js

--- src/pages/plan.js
+++ src/pages/plan.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import Hero from '../components/Hero.js';
 import Button from '../components/Button.js';
 import IconClose from '../images/shared/mobile/icon-close.svg';
